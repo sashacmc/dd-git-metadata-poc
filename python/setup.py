f066504512103ac2ab25ccefd3a9cd5bb9295fbf
Alexander Bushnev <aleksandr.bushnev@datadoghq.com>
2022-11-10 18:43:16 +0100

    Add dummy setup file

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Test',
+      version='1.0',
+      description='Python Test',
+      author='First Last',
+      author_email='example@mail.net',
+     )
