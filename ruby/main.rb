77010390054db45cd5254a31e92c2793e6e9c13b
Alexander Bushnev <aleksandr.bushnev@datadoghq.com>
2022-11-30 16:36:11 +0100

    Add ruby tagging

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,7 +1,22 @@
-require 'ddtrace'
+require "ddtrace"
 
-require 'ddtrace/auto_instrument'
+require "ddtrace/auto_instrument"
+
+def get_commit_sha
+  `git rev-parse HEAD 2>/dev/null`.strip
+rescue StandardError
+end
+
+def get_repository_url
+  `git config --get remote.origin.url 2>/dev/null`.strip
+rescue StandardError
+end
+
+Datadog.configure do |c|
+  c.tags = { "git.commit.sha" => get_commit_sha(),
+             "git.repository_url" => get_repository_url() }
+end
 
 Datadog::Tracing.trace("gitmetadatapoc", resource: "main-resource") do |span|
-	puts "gitmetadatapoc runs"
+  puts "gitmetadatapoc runs"
 end
