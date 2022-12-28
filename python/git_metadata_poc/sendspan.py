6e74ed860ee8c4d30b9e87542afde02465546c8b
Alexander Bushnev <aleksandr.bushnev@datadoghq.com>
2022-12-28 18:42:21 +0100

    Add python profiler call

--- python/git_metadata_poc/sendspan.py
+++ python/git_metadata_poc/sendspan.py
@@ -1,4 +1,14 @@
 import ddtrace
 
-with ddtrace.tracer.trace("gitmetadata.test.span"):
-    raise Exception("some error")
+from ddtrace.profiling import Profiler
+
+prof = Profiler()
+prof.start()
+
+def send_span():
+    with ddtrace.tracer.trace("gitmetadata.test.span"):
+        raise Exception("some error")
+
+
+if __name__ == "__main__":
+    send_span()
