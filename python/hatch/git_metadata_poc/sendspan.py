19e87063c1a02f276af06babbde43b79aa4b1533
Alexander Bushnev <aleksandr.bushnev@datadoghq.com>
2023-03-31 15:44:45 +0200

    Update hatch setup

--- python/hatch/git_metadata_poc/sendspan.py
+++ python/hatch/git_metadata_poc/sendspan.py
@@ -5,9 +5,10 @@ from ddtrace.profiling import Profiler
 prof = Profiler()
 prof.start()
 
+
 def send_span():
-    with ddtrace.tracer.trace("gitmetadata.test.span"):
-        raise Exception("some error")
+    with ddtrace.tracer.trace("gitmetadataH.test.span"):
+        raise Exception("some error H")
 
 
 if __name__ == "__main__":
